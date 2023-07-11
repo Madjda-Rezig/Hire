f4a2936a9c66bb5b5054d6e0260f0e008f6e39d7
Madjda-Rezig <rezigmadjda@yahoo.com>
2023-07-11 11:04:53 +0100

    contact us model

---
+++ Backend/models/contactModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+        type: String,
+        required: true,
+        
+        
+      },
+      mail: {
+        type: String,
+        required: true,
+        
+        
+      },
+      message: {
+        type: String,
+        required: true,
+    
+        
+      },
+   
+    date_creation: {
+      type: Date,
+      default: Date.now,
+    },
+   
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("contactus", contactSchema);
