29c607ee7fb4e00da8278538edaa026d39674735
Madjda-Rezig <rezigmadjda@yahoo.com>
2023-07-11 11:29:19 +0100

    contact us Route

---
+++ Backend/routes/contactRoute.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {
+    sendMessage,
+    getAllMessages
+} = require("../controllers/contactController")
+
+const  {protectRecruteur, protectCandidat,protectUser} = require('../middlewares/Protect')
+const contactRouter = require("express").Router()
+
+contactRouter
+
+.post("/contactus", sendMessage )
+.get("/all",getAllMessages)
+
+
+module.exports = newsletterRouter;
