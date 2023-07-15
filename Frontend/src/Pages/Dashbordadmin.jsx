08ddae518e8b9c544a743ee9e2d4d1f64962e5e0
Madjda-Rezig <rezigmadjda@yahoo.com>
2023-07-15 20:45:27 +0100

    Dashbord Admin

--- Frontend/src/Pages/Dashbordadmin.jsx
+++ Frontend/src/Pages/Dashbordadmin.jsx
@@ -1,3 +1,212 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+
 export default function Dashbordadmin() {
-  return <div></div>;
+  const [nom, setNom] = useState("");
+  const [prenom, setPrenom] = useState("");
+  const [mail, setMail] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const user = localStorage.getItem("User");
+        const { accessToken, _id } = JSON.parse(user);
+        const response = await axios.get(`http://localhost:5000/users/${_id}`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        setNom(response.data.nom);
+        setPrenom(response.data.prenom);
+        setMail(response.data.mail);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchProfile();
+  }, []);
+
+  const handleLogout = async () => {
+    try {
+      const user = localStorage.getItem("User");
+      const token = JSON.parse(user).accessToken;
+      await axios.delete(`http://localhost:5000/auth/logout/${token}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      localStorage.removeItem("User");
+      navigate("/Admin");
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+  return (
+    <div>
+      <div className="flex h-screen flex-col justify-between border-e bg-white w-96">
+        <div className="px-4 py-6 ">
+          <div className="text-center">
+            <span className=" my-10 text-center sm:text-5xl text-4xl font-bold bg-gradient-to-r from-blue-300 via-[#1CD2B1] to-blue-600 bg-clip-text text-transparent ">
+              WorkUP
+            </span>
+          </div>
+          <ul className="mt-6 space-y-1">
+            <li>
+              <a
+                href=""
+                className="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
+              >
+                Statistique
+              </a>
+            </li>
+
+            <li>
+              <a
+                href=""
+                className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+              >
+                Profil
+              </a>
+            </li>
+
+            <li>
+              <details className="group [&_summary::-webkit-details-marker]:hidden">
+                <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+                  <span className="text-sm font-medium"> Offres </span>
+
+                  <span className="shrink-0 transition duration-300 group-open:-rotate-180">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </span>
+                </summary>
+
+                <ul className="mt-2 space-y-1 px-4">
+                  <li>
+                    <a
+                      href=""
+                      className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                    >
+                      Add an Offer
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      href=""
+                      className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                    >
+                      Show all Offers
+                    </a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+
+            <li>
+              <details className="group [&_summary::-webkit-details-marker]:hidden">
+                <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+                  <span className="text-sm font-medium"> Candidatures </span>
+
+                  <span className="shrink-0 transition duration-300 group-open:-rotate-180">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </span>
+                </summary>
+
+                <ul className="mt-2 space-y-1 px-4">
+                  <li>
+                    <a
+                      href=""
+                      className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                    >
+                      Details
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      href=""
+                      className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                    >
+                      Security
+                    </a>
+                  </li>
+
+                  <li>
+                    <form action="/logout">
+                      <button
+                        type="submit"
+                        className="w-full rounded-lg px-4 py-2 text-sm font-medium text-gray-500 [text-align:_inherit] hover:bg-gray-100 hover:text-gray-700"
+                      >
+                        Logout
+                      </button>
+                    </form>
+                  </li>
+                </ul>
+              </details>
+              <hr className="border-gray-200 my-4" />
+              <li>
+                <a
+                  onClick={handleLogout}
+                  className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                >
+                  <FontAwesomeIcon icon={faSignOutAlt} className="mr-2" />
+                  Logout
+                </a>
+              </li>
+            </li>
+          </ul>
+        </div>
+
+        <div className="sticky inset-x-0 bottom-0 border-t border-gray-100">
+          <a
+            href="#"
+            className="flex items-center gap-2 bg-white p-4 hover:bg-gray-50"
+          >
+            <img
+              alt="Man"
+              src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+              className="h-10 w-10 rounded-full object-cover"
+            />
+
+            <div>
+              <p className="text-xs">
+                <strong className="block font-medium">
+                  {prenom} {nom}
+                </strong>
+
+                <span> {mail} </span>
+              </p>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
