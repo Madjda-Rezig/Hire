a228cd7ad4596dac7ea06ad6e6f1ad6a81f7873d
Madjda-Rezig <rezigmadjda@yahoo.com>
2023-07-20 21:10:58 +0100

    DashbordAdmin/frontend add article

--- Frontend/src/Pages/Addarticle.jsx
+++ Frontend/src/Pages/Addarticle.jsx
@@ -60,6 +60,9 @@ const AddArticle = () => {
     <div className="hero min-h-screen bg-base-200">
       <div className="bg-white border-2 border-gray-200 shadow-xl pt-10 pb-14 sm:w-4/5 md:w-3/5 lg:w-3/5 rounded-lg mt-4 mb-10 grid place-items-center">
         <form className="w-4/5" onSubmit={handleSubmit}>
+          <h2 className="mt-2 text-2xl font-bold sm:text-4xl text-center bg-gradient-to-r from-blue-300 via-[#1CD2B1] to-blue-600 text-white mb-5">
+            Add an Article
+          </h2>
           <div className="w-full flex gap-2 items-center">
             <div className="form-control w-full ">
               <label className="label">
@@ -151,7 +154,7 @@ const AddArticle = () => {
 
           <button
             type="submit"
-            className="btn w-full mt-14 border-none bg-blue-600"
+            className="btn w-full mt-14 border-none bg-gradient-to-r from-blue-600 via-[#1CD2B1] to-green-300"
             disabled={loading}
           >
             Add Article
